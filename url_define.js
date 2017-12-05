87e382f5b8674b8a5a9be61e4ba057c521a642de
kay007 <kay61515@gamil.com>
2017-12-05 10:03:44 +0800

    try
    try

---
+++ url_define.js
@@ -0,0 +1,8 @@
+//即時路口影像
+var home_traffic = "https://app.powerbi.com/view?r=eyJrIjoiNTIwMDRhNzktZmE2OS00Yzg1LWI4NjEtNjZmMjQ5NmI1NWYzIiwidCI6IjUyZjNmOGE1LTc2ZGYtNGJhZC1iZTg5LTU2OWVhMDY2NGRmNCIsImMiOjEwfQ%3D%3D";
+
+//執行機關派工件數前三名
+var top_three = "https://app.powerbi.com/view?r=eyJrIjoiYjExNmQ5ZGUtOTI2MS00ODA0LWJmYzgtMmIzMGNkYWZjZWY3IiwidCI6IjUyZjNmOGE1LTc2ZGYtNGJhZC1iZTg5LTU2OWVhMDY2NGRmNCIsImMiOjEwfQ%3D%3D";
+
+//iTaiwan Wifi
+var iTaiwan_Wifi = "https://app.powerbi.com/view?r=eyJrIjoiZWNmNDU1NGEtZWZmOC00MzBjLThiYTItYzQwMGVkM2YzMTFmIiwidCI6IjUyZjNmOGE1LTc2ZGYtNGJhZC1iZTg5LTU2OWVhMDY2NGRmNCIsImMiOjEwfQ%3D%3D";
